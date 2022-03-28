e491587a334f2101edc6186a13c23c02956203ef
Degurshaff <ilyaexe666@gmail.com>
2022-03-29 04:35:49 +0700

    w

---
+++ ContactsApp/ContactsAppUI/AboutForm.Designer.cs
@@ -0,0 +1,196 @@
+using System.ComponentModel;
+
+namespace ContactsAppUI
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DiscordLink = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.GitHubLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.EmaleLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.AutorBottomLabel = new System.Windows.Forms.Label();
+            this.GitHubLabel = new System.Windows.Forms.Label();
+            this.EmailLabel = new System.Windows.Forms.Label();
+            this.AutorLabel = new System.Windows.Forms.Label();
+            this.VersionLabel = new System.Windows.Forms.Label();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // DiscordLink
+            // 
+            this.DiscordLink.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left)));
+            this.DiscordLink.AutoSize = true;
+            this.DiscordLink.Location = new System.Drawing.Point(125, 201);
+            this.DiscordLink.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.DiscordLink.Name = "DiscordLink";
+            this.DiscordLink.Size = new System.Drawing.Size(148, 13);
+            this.DiscordLink.TabIndex = 22;
+            this.DiscordLink.TabStop = true;
+            this.DiscordLink.Text = "https://vk.com/ilyadegurshaff";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 201);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "VK:";
+            // 
+            // GitHubLinkLabel
+            // 
+            this.GitHubLinkLabel.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left)));
+            this.GitHubLinkLabel.AutoSize = true;
+            this.GitHubLinkLabel.Location = new System.Drawing.Point(125, 174);
+            this.GitHubLinkLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.GitHubLinkLabel.Name = "GitHubLinkLabel";
+            this.GitHubLinkLabel.Size = new System.Drawing.Size(152, 13);
+            this.GitHubLinkLabel.TabIndex = 20;
+            this.GitHubLinkLabel.TabStop = true;
+            this.GitHubLinkLabel.Text = "https://github.com/Degurshaff";
+            // 
+            // EmaleLinkLabel
+            // 
+            this.EmaleLinkLabel.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left)));
+            this.EmaleLinkLabel.AutoSize = true;
+            this.EmaleLinkLabel.Location = new System.Drawing.Point(125, 148);
+            this.EmaleLinkLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.EmaleLinkLabel.Name = "EmaleLinkLabel";
+            this.EmaleLinkLabel.Size = new System.Drawing.Size(109, 13);
+            this.EmaleLinkLabel.TabIndex = 19;
+            this.EmaleLinkLabel.TabStop = true;
+            this.EmaleLinkLabel.Text = "ilyamonro@gmail.com\r\n";
+            // 
+            // AutorBottomLabel
+            // 
+            this.AutorBottomLabel.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.AutorBottomLabel.AutoSize = true;
+            this.AutorBottomLabel.Location = new System.Drawing.Point(125, 338);
+            this.AutorBottomLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.AutorBottomLabel.Name = "AutorBottomLabel";
+            this.AutorBottomLabel.Size = new System.Drawing.Size(85, 13);
+            this.AutorBottomLabel.TabIndex = 18;
+            this.AutorBottomLabel.Text = "2022 Karelin Ilya";
+            // 
+            // GitHubLabel
+            // 
+            this.GitHubLabel.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left)));
+            this.GitHubLabel.AutoSize = true;
+            this.GitHubLabel.Location = new System.Drawing.Point(79, 174);
+            this.GitHubLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.GitHubLabel.Name = "GitHubLabel";
+            this.GitHubLabel.Size = new System.Drawing.Size(43, 13);
+            this.GitHubLabel.TabIndex = 17;
+            this.GitHubLabel.Text = "GitHub:";
+            // 
+            // EmailLabel
+            // 
+            this.EmailLabel.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left)));
+            this.EmailLabel.AutoSize = true;
+            this.EmailLabel.Location = new System.Drawing.Point(87, 148);
+            this.EmailLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.EmailLabel.Name = "EmailLabel";
+            this.EmailLabel.Size = new System.Drawing.Size(35, 13);
+            this.EmailLabel.TabIndex = 16;
+            this.EmailLabel.Text = "Email:";
+            // 
+            // AutorLabel
+            // 
+            this.AutorLabel.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left)));
+            this.AutorLabel.AutoSize = true;
+            this.AutorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.AutorLabel.Location = new System.Drawing.Point(79, 94);
+            this.AutorLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.AutorLabel.Name = "AutorLabel";
+            this.AutorLabel.Size = new System.Drawing.Size(89, 13);
+            this.AutorLabel.TabIndex = 15;
+            this.AutorLabel.Text = "Autor: Karelin Ilya";
+            // 
+            // VersionLabel
+            // 
+            this.VersionLabel.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left)));
+            this.VersionLabel.AutoSize = true;
+            this.VersionLabel.Location = new System.Drawing.Point(81, 38);
+            this.VersionLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.VersionLabel.Name = "VersionLabel";
+            this.VersionLabel.Size = new System.Drawing.Size(41, 13);
+            this.VersionLabel.TabIndex = 14;
+            this.VersionLabel.Text = "V 0.0.1";
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left)));
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.NameLabel.Location = new System.Drawing.Point(79, 9);
+            this.NameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(147, 29);
+            this.NameLabel.TabIndex = 13;
+            this.NameLabel.Text = "ContactApp";
+            // 
+            // AboutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 360);
+            this.Controls.Add(this.DiscordLink);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GitHubLinkLabel);
+            this.Controls.Add(this.EmaleLinkLabel);
+            this.Controls.Add(this.AutorBottomLabel);
+            this.Controls.Add(this.GitHubLabel);
+            this.Controls.Add(this.EmailLabel);
+            this.Controls.Add(this.AutorLabel);
+            this.Controls.Add(this.VersionLabel);
+            this.Controls.Add(this.NameLabel);
+            this.MinimumSize = new System.Drawing.Size(330, 370);
+            this.Name = "AboutForm";
+            this.Text = "AboutForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.LinkLabel DiscordLink;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel GitHubLinkLabel;
+        private System.Windows.Forms.LinkLabel EmaleLinkLabel;
+        private System.Windows.Forms.Label AutorBottomLabel;
+        private System.Windows.Forms.Label GitHubLabel;
+        private System.Windows.Forms.Label EmailLabel;
+        private System.Windows.Forms.Label AutorLabel;
+        private System.Windows.Forms.Label VersionLabel;
+        private System.Windows.Forms.Label NameLabel;
+
+        #endregion
+    }
+}
